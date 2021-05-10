b87d31dcdf87d8c0d372509d9d71a96d68e668db
Miller1998 <33412924+BezaleelAnugrahri@users.noreply.github.com>
2021-05-10 21:34:47 +0700

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 mousePosition;
+
+    public Camera cam;
+
+    [SerializeField]
+    private NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseOnClickPosition();
+        }
+
+    }
+
+
+    void MouseOnClickPosition()
+    {
+        mousePosition = Input.mousePosition;
+        Ray ray = cam.ScreenPointToRay(mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Character will go to the mouse on click position
+            agent.SetDestination(hit.point);
+        }
+
+    }
+
+}
