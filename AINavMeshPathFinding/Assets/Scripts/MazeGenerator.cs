09083ee92a06c7e8b9192dd13c5ab5679c1e1d86
Miller1998 <33412924+BezaleelAnugrahri@users.noreply.github.com>
2021-05-25 12:39:10 +0700

    Advance Navmesh Tutorial
    Making Navmesh more dynamic and flexible

---
+++ AINavMeshPathFinding/Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MazeGenerator : MonoBehaviour
+{
+	public int width = 10;
+	public int height = 10;
+
+	public GameObject wall;
+	public GameObject player;
+	public GameObject parent;
+
+	public NavMeshSurface surface;
+
+
+	private bool playerSpawned = false;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		GenerateLevel();
+
+		//Update NavMesh 
+		surface.BuildNavMesh();
+
+	}
+
+	// Create a grid based level
+	void GenerateLevel()
+	{
+		// Loop over the grid
+		for (int x = 0; x <= width; x += 2)
+		{
+			for (int y = 0; y <= height; y += 2)
+			{
+				// Should we place a wall?
+				if (Random.value > .7f)
+				{
+					// Spawn a wall
+					Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
+					Instantiate(wall, pos, Quaternion.identity, transform);
+				}
+				else if (!playerSpawned) // Should we spawn a player?
+				{
+					// Spawn the player
+					Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
+					Instantiate(player, pos, Quaternion.identity, parent.transform);
+					playerSpawned = true;
+				}
+			}
+		}
+	}
+}
---
+++ AINavMeshPathFinding/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 touchPos;
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Mouse.current.leftButton.isPressed)
+        {
+            MouseOnClickPosition();
+        }
+        //MouseOnClickPosition();
+
+    }
+
+
+    void MouseOnClickPosition()
+    {
+
+        //touchPos = Input.mousePosition;//if you not using new input system
+        touchPos = Mouse.current.position.ReadValue();//Getting Mouse Position in screen coordinate and we can using New Input systems (you can get it from Package Manager)
+        Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(touchPos);//convert mouse pos on screen to ray that we can shoot out and convert it to the dir that we click
+        RaycastHit rayCastHit;
+
+        if (Physics.Raycast(ray/*ray that we want to shoot*/, 
+            out rayCastHit/*information what we hit and where it hit and store it inside our rayCastHit var*/))
+            //if our ray hit something not an object then we wont do anything, but if it hit object then
+        {
+
+            //Move Our Agent to the mouse point of click Position
+            agent.SetDestination(rayCastHit.point);
+
+        }
+
+    }
+
+}
